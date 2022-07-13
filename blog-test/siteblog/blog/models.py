7ee0fd58c46bbddc60420179961e5fd49c1bce66
altec05 <domashenko213@gmail.com>
2022-07-13 14:19:14 +0700

    Финал написания блога
    Готов к изучению деплоя сайта.

---
+++ blog-test/siteblog/blog/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.urls import reverse
+
+'''
+Category
+=========
+title, slug
+
+Tag
+=========
+title, slug
+
+Post
+=========
+title, slug, author, content, cteated_at, photo, views, category, tags
+'''
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Изображение')
+    views = models.IntegerField(default=0, verbose_name='Просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Тэги')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
